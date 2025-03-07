96a732f944a26600e0db7626e52f8899024b360d
tanvir.sajib <34455693+tanvirlaravel@users.noreply.github.com>
2025-03-07 20:53:25 +0600

    AddTodo retrun text

--- src/1-to-do/AddTodo.jsx
+++ src/1-to-do/AddTodo.jsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 function AddTodo({ onAdd }) {
+  const [text, setText] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (text.trim()) {
+      onAdd(text);
+      setText('');
+    }
+  }
   return (
-    <form className="add-todo">
-      <input type="text" placeholder="Add a new task..." />
-      <button>Add</button>
+    <form className="add-todo" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a new task..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button type="submit">Add</button>
     </form>
   );
 }
--- src/1-to-do/ToDoApp.jsx
+++ src/1-to-do/ToDoApp.jsx
@@ -4,21 +4,21 @@ import './todo.css';
 import AddTodo from './AddTodo';
 
 function ToDoApp() {
-  const [todos, setTodos ] = useState([
+  const [todos, setTodos] = useState([
     { id: 1, text: 'Learn React', completed: false },
     { id: 2, text: 'Build a To-Do App', completed: true },
     { id: 3, text: 'Deploy the App', completed: false },
   ]);
 
-
-  function addTodo(){}
-  
+  function addTodo(text) {
+    console.log(text);
+  }
 
   return (
     <div className="app">
       <h1>Todo App</h1>
       <AddTodo onAdd={addTodo} />
-      <TodoList todos={todos}  />
+      <TodoList todos={todos} />
     </div>
   );
 }
