813b0a770555222daf84c0ba2d721874eec36934
tanvir.sajib <34455693+tanvirlaravel@users.noreply.github.com>
2025-03-08 13:55:43 +0600

    reducer added

--- src/3-Expense-Tracker/AppExpenseTracker.jsx
+++ src/3-Expense-Tracker/AppExpenseTracker.jsx
@@ -2,16 +2,25 @@ import './styles.css';
 import { mockExpenses } from './mockExpenses';
 import ExpenseForm from './ExpenseForm';
 import ExpenseList from './ExpenseList';
+import { useReducer } from 'react';
+import expenseReducer from './expenseReducer';
 
-
+// Initial state
+const initialState = {
+  expenses: [],
+  filter: 'All',
+};
 
 function AppExpenseTracker() {
+  const [state, dispatch] = useReducer(expenseReducer, initialState);
+
+  console.log('state', state);
+  console.log( "dispatch", dispatch);
   return (
     <div>
       <h1>App Expense Tracker</h1>
       <ExpenseForm />
-      <ExpenseList expenses ={mockExpenses} />
-     
+      <ExpenseList expenses={mockExpenses} />
     </div>
   );
 }
---
+++ src/3-Expense-Tracker/expenseReducer.jsx
@@ -0,0 +1,12 @@
+export default function expenseReducer(state, action) {
+    switch (action.type) {
+      case 'ADD_EXPENSE':
+        return { ...state, expenses: [...state.expenses, action.payload] };
+      case 'DELETE_EXPENSE':
+        return { ...state, expenses: state.expenses.filter(exp => exp.id !== action.payload) };
+      case 'SET_FILTER':
+        return { ...state, filter: action.payload };
+      default:
+        return state;
+    }
+  }
