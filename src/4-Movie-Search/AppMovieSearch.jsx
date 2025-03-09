3c0addbffda2c0fbd86d495907cdd1cd117c9e0b
tanvir.sajib <34455693+tanvirlaravel@users.noreply.github.com>
2025-03-09 13:55:29 +0600

    loading working

--- src/4-Movie-Search/AppMovieSearch.jsx
+++ src/4-Movie-Search/AppMovieSearch.jsx
@@ -1,17 +1,20 @@
 import { useEffect, useState } from 'react';
-import { mockMovies } from './mockMovies';
-import { API_KEY, BASE_URL, keyword, page } from './CONST_VALUE';
+import { mockMovies, omdbApiMovies } from './mockMovies';
+import { API_KEY, BASE_URL, keyword } from './CONST_VALUE';
 import MovieList from './MovieList';
 import SearchBox from './SearchBox';
 import './style.css';
 
 function AppMovieSearch() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
   const [totalMovie, setTotalMovie] = useState(0);
+  
+  const [page, setPage] = useState(2);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       try {
         const response = await fetch(
@@ -25,19 +28,33 @@ function AppMovieSearch() {
       }
     };
 
-    fetchData();
-  }, [keyword, page]);
+    // fetchData();
 
-  if (movies) {
-    console.log('movies', movies);
-  }
+    setTimeout(() => {
+      setTotalMovie(omdbApiMovies.totalResults);
+      setMovies(omdbApiMovies.Search);
+      setIsLoading(false);
+    }, 1000);
+  }, [keyword, page]);
 
   return (
     <div>
       <h1>AppMovieSearch</h1>
+
       <SearchBox />
-      <h2>Showing movies {movies.length} of total: {totalMovie}</h2>
-      <MovieList movies={movies} />
+
+     
+
+      {isLoading && <h2>Loading...</h2>}
+
+      {!isLoading && (
+        <>
+          <h2>
+            Showing movies {movies.length} of total: {totalMovie}
+          </h2>
+          <MovieList movies={movies} />
+        </>
+      )}
     </div>
   );
 }
